d036acafd97a9b3851e55e0d189ef1b5e7063412
Michael W <michaelw.apple@icloud.com>
2022-01-17 14:04:40 +0100

    node-sass added

--- backend/src/main/java/de/neuefische/backend/controller/ShoppingController.java
+++ backend/src/main/java/de/neuefische/backend/controller/ShoppingController.java
@@ -1,7 +1,6 @@
 package de.neuefische.backend.controller;
 
 import de.neuefische.backend.repository.ShoppingRepo;
-import de.neuefische.backend.service.ShoppingService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +8,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class ShoppingController {
 
-    final ShoppingRepo shoppingRepo;
-
-    public ShoppingController(ShoppingRepo shoppingRepo) {
-        this.shoppingRepo = shoppingRepo;
     }
-}
