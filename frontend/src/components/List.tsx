3451783425e2eabc411ea30cba1b9fc14e5abf32
Michael W <michaelw.apple@icloud.com>
2022-01-21 10:30:46 +0100

    create branch, first push

--- frontend/src/components/List.tsx
+++ frontend/src/components/List.tsx
@@ -16,34 +16,34 @@ export default function List({itemList, setItemList}:{itemList:Product[],setItem
 // Das gewünschte Feature. Button mit "Count" funktion.
     // Vielleicht doch mit Mapping?
 
-    interface Icount{
-        count : 1
-    }
-
-    const [itemCount, setItemCount] = useState(1
-
-    )
-    const increaseCount = (key: number) => {
-        const newList = [...itemList];
-        newList[key].count++;
-        setItemList(newList)
-        console.log(newList)
-    }
-
-    const decreaseCount = (key:number) => {
-        const newList    = [...itemList];
-        newList[key].count--;
-        setItemList(newList)
-        console.log(newList)
-    }
+    // interface Icount{
+    //     count : 1
+    // }
+    //
+    // const [itemCount, setItemCount] = useState(1
+    //
+    // )
+    // const increaseCount = (key: number) => {
+    //     const newList = [...itemList];
+    //     newList[key].count++;
+    //     setItemList(newList)
+    //     console.log(newList)
+    // }
+    //
+    // const decreaseCount = (key:number) => {
+    //     const newList    = [...itemList];
+    //     newList[key].count--;
+    //     setItemList(newList)
+    //     console.log(newList)
+    // }
     return (
         <div>
             {itemList.map((itemObj) => {
                 return <div key={itemObj.key} className="Item">
                     <p>{itemObj.name} Anzahl: {itemObj.count}</p>
                     <button onClick={() => removeItem(itemObj.key)}>Remove</button>
-                    <button onClick={() => increaseCount(itemObj.key)}>+</button>
-                    <button onClick={() => decreaseCount(itemObj.key)}>-</button>
+                    {/*<button onClick={() => increaseCount(itemObj.key)}>+</button>*/}
+                    {/*<button onClick={() => decreaseCount(itemObj.key)}>-</button>*/}
                 </div>
             })}
         </div>
